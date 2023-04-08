f656375d58358803d38a473a3750285fedc6fecb
Soldem <57632160+Soldem@users.noreply.github.com>
2023-04-08 11:59:33 +0300

    Add files via upload
    adding task 1

---
+++ ReverseNumber/ReverseNumber/ReverseNumber.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long number;
+
+    cin >> number;
+
+    unsigned long long reverse = 0;
+
+    while (number > 0)
+    {
+        reverse *= 10;
+        reverse += number % 10;
+        number /= 10;
+    }
+
+    cout << reverse << endl;
+
+    system("pause");
+}
+
