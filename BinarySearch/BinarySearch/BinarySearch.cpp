e4bb629719a06abe68af3ee93eef78c4155ca344
Soldem <57632160+Soldem@users.noreply.github.com>
2023-04-08 12:14:09 +0300

    Add files via upload
    add task 4

---
+++ BinarySearch/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int* MAS;
+int SIZE;
+
+bool BinarySearch(const int* begin, const int* end, int target)
+{
+    int left = *begin;
+    int rigth = *end;
+    while (left < rigth - 1)
+    {
+        int middle = (left + rigth) / 2;
+        if (MAS[middle] == target) return true;
+
+        if (MAS[middle] < target) left = middle;
+        else rigth = middle;
+    }
+    if (MAS[left] == target) return true;
+    else return false;
+}
+
+int main()
+{
+    int q, i, j, t;
+    std::cin >> SIZE;
+
+    MAS = new int[SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        std::cin >> MAS[i];
+    }
+
+    std::cin >> q;
+    for (int k = 0; k < q; k++)
+    {
+        std::cin >> i >> j >> t;
+        std::cout << (BinarySearch(&i, &j, t) ? "YES" : "NO") << std::endl;
+    }
+}
+
