496e80abace47ea24a61c3ca1fe1129f001be4de
Soldem <57632160+Soldem@users.noreply.github.com>
2023-04-09 23:22:23 +0300

    Update ReverseNumber.cpp

--- ReverseNumber/ReverseNumber/ReverseNumber.cpp
+++ ReverseNumber/ReverseNumber/ReverseNumber.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     unsigned long long number;
     cin >> number;
     unsigned long long reverse = 0;
-    for(; number > 0; )
-    {
+    for(; number > 0; ){
         reverse *= 10;
         reverse += number % 10;
         number /= 10;
