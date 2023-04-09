676dcf99a7de279f7469884658f3e727e58dd706
Soldem <57632160+Soldem@users.noreply.github.com>
2023-04-09 23:28:39 +0300

    Update BinarySearch.cpp

--- BinarySearch/BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch/BinarySearch.cpp
@@ -1,14 +1,10 @@
 ﻿#include <iostream>
-
 int* MAS;
 int SIZE;
-
-bool BinarySearch(const int* begin, const int* end, int target)
-{
+bool BinarySearch(const int* begin, const int* end, int target){
     int left = *begin;
     int rigth = *end;
-    while (left < rigth - 1)
-    {
+    while (left < rigth - 1){
         int middle = (left + rigth) / 2;
         if (MAS[middle] == target) return true;
 
@@ -19,22 +15,16 @@ bool BinarySearch(const int* begin, const int* end, int target)
     else return false;
 }
 
-int main()
-{
+int main(){
     int q, i, j, t;
     std::cin >> SIZE;
-
     MAS = new int[SIZE];
-    for (int i = 0; i < SIZE; i++)
-    {
+    for (int i = 0; i < SIZE; i++){
         std::cin >> MAS[i];
     }
-
     std::cin >> q;
-    for (int k = 0; k < q; k++)
-    {
+    for (int k = 0; k < q; k++){
         std::cin >> i >> j >> t;
         std::cout << (BinarySearch(&i, &j, t) ? "YES" : "NO") << std::endl;
     }
 }
-
