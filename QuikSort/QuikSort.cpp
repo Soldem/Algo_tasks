315eab4a999a80b3d31d90fe92947f40d7d2c6fe
Soldem <57632160+Soldem@users.noreply.github.com>
2023-06-08 20:27:40 +0300

    Update QuikSort.cpp

--- QuikSort/QuikSort.cpp
+++ QuikSort/QuikSort.cpp
@@ -2,38 +2,38 @@
 #include <iostream>
 #include <vector>
 
-void Quik_sort(int* s_arr, int first, int last) {
-  if (first < last) {
-    int left = first, right = last, middle = s_arr[(left + right) / 2];
-    do {
-      while (s_arr[left] < middle) left++;
-      while (s_arr[right] > middle) right--;
-      if (left <= right) {
-        int tmp = s_arr[left];
-        s_arr[left] = s_arr[right];
-        s_arr[right] = tmp;
-        left++;
-        right--;
-      }
-    } while (left <= right);
-    Quik_sort(s_arr, first, right);
-    Quik_sort(s_arr, left, last);
-  }
+void quik_sort(int* s_arr, int first, int last) {
+    if (first < last) {
+        int left = first, right = last, middle = s_arr[(left + right) / 2];
+        do {
+            while (middle > s_arr[left]) left++;
+            while (middle < s_arr[right]) right--;
+            if (left <= right) {
+                int tmp = s_arr[left];
+                s_arr[left] = s_arr[right];
+                s_arr[right] = tmp;
+                left++;
+                right--;
+            }
+        } while (left <= right);
+        quik_sort(s_arr, first, right);
+        quik_sort(s_arr, left, last);
+    }
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  int* a = new int[n];
+    int n;
+    std::cin >> n;
+    int* a = new int[n];
 
-  for (int i = 0; i < n; i++) {
-    std::cin >> a[i];
-  }
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
 
-  Quik_sort(a, 0, n - 1);
-  for (int i = 0; i < n; i++) {
-    std::cout << a[i] << " ";
-  }
+    quik_sort(a, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        std::cout << a[i] << " ";
+    }
 
-  delete[] a;
+    delete[] a;
 }
